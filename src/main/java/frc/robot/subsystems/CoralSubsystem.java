3139bdec402e0076092891d5cb8cffa2c53754cb
James Friggens <jfriggs1877@gmail.com>
2025-01-14 20:07:35 -0500

    Create coral intake and arm subsystem

---
+++ src/main/java/frc/robot/subsystems/CoralSubsystem.java
@@ -0,0 +1,49 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.spark.SparkAbsoluteEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+public class CoralSubsystem extends SubsystemBase {
+
+    private SparkMax pivotMotor;
+    private SparkMax intakeMotor;
+    private SparkAbsoluteEncoder throughBoreEncoder;
+
+    public CoralSubsystem() {
+
+        pivotMotor = new SparkMax(71, MotorType.kBrushless);
+        throughBoreEncoder = pivotMotor.getAbsoluteEncoder();
+        intakeMotor = new SparkMax(72, MotorType.kBrushless);
+    }
+
+    public void setPivotMotorSpeed(double speed) {
+        pivotMotor.set(speed);
+    }
+
+    public void stopPivotMotor() {
+        pivotMotor.stopMotor();
+    }
+
+    public double getPivotPosition() {
+        return throughBoreEncoder.getPosition();
+    }
+
+    public double getPivotVelocity() {
+        return throughBoreEncoder.getVelocity();
+    }
+
+    public void setIntakeMotorSpeed(double speed) {
+        intakeMotor.set(speed);
+    }
+
+    public void stopIntakeMotor() {
+        intakeMotor.stopMotor();
+    }
+
+    public double getIntakeCurrent() {
+        return intakeMotor.getOutputCurrent();
+    }
+}
