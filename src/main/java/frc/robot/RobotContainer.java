e3cb251817b01a789e25c83fcf739cc36da51a13
James Friggens <jfriggs1877@gmail.com>
2025-01-16 13:43:37 -0500

    Add 4 Coral Auto to Autonomous Picker

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -160,7 +160,7 @@ public class RobotContainer
   public Command getAutonomousCommand()
   {
     // An example command will be run in autonomous
-    return drivebase.getAutonomousCommand("New Auto");
+    return drivebase.getAutonomousCommand("4 Coral Red Barge");
   }
 
   public void setMotorBrake(boolean brake)
