53a1171b3fa149f0e77602f9ac2868aa0b711877
James Friggens <jfriggs1877@gmail.com>
2025-01-22 13:14:48 -0500

    Create enum for position control Create method for onboard REV PID controller Update and install vendordeps

--- src/main/java/frc/robot/subsystems/CoralSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralSubsystem.java
@@ -3,8 +3,13 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.revrobotics.spark.SparkAbsoluteEncoder;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkBase.ResetMode;
 
 public class CoralSubsystem extends SubsystemBase {
 
@@ -12,11 +17,39 @@ public class CoralSubsystem extends SubsystemBase {
     private SparkMax intakeMotor;
     private SparkAbsoluteEncoder throughBoreEncoder;
 
-    public CoralSubsystem() {
+    private SparkClosedLoopController pivotMotorPID;
+    private SparkMaxConfig pivotMotorConfig;
 
-        pivotMotor = new SparkMax(71, MotorType.kBrushless);
-        throughBoreEncoder = pivotMotor.getAbsoluteEncoder();
+    public enum PivotPositions {
+        L1(0),
+        L2(0),
+        L3(0),
+        L4(0);
+
+        private final double value;
+
+        PivotPositions(double value) {
+            this.value = value;
+        }
+
+        public double getValue() {
+            return value;
+        }
+
+    }
+
+    public CoralSubsystem() {
         intakeMotor = new SparkMax(72, MotorType.kBrushless);
+        pivotMotor = new SparkMax(71, MotorType.kBrushless);
+        pivotMotorPID = pivotMotor.getClosedLoopController();
+        pivotMotorConfig = new SparkMaxConfig();
+        throughBoreEncoder = pivotMotor.getAbsoluteEncoder();
+
+        pivotMotorConfig.closedLoop
+                .p(0.001)
+                .d(0)
+                .outputRange(-1, 1);
+        pivotMotor.configure(pivotMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     }
 
     public void setPivotMotorSpeed(double speed) {
@@ -49,5 +82,9 @@ public class CoralSubsystem extends SubsystemBase {
 
     public double getIntakeTemperature() {
         return intakeMotor.getMotorTemperature();
+    }
+
+    public void pidSetPosition(PivotPositions position) {
+        pivotMotorPID.setReference(position.getValue(), ControlType.kPosition);
     }
 }
